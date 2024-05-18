9d6bf608a02cc98d2885a5ad661756589e9f23fe
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-18 04:40:19 -0700

    code for 3-hypermedia_del_pagination.py

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names
+    """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """
+        Dataset indexed by sorting position, starting as 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        """Get hypermedia pagination
+        """
+        assert isinstance(index, int) and index >= 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        indexed_data = self.indexed_dataset()
+        assert index < len(indexed_data)
+
+        data = [indexed_data[i] for i in range(index, index + page_size) if i in indexed_data]
+        next_index = max(i for i in indexed_data if i >= index and i < index + page_size) + 1
+
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data
+        }
