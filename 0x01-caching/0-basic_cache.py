4d43ef65a76a4d03ef75d51b155d6148bf3b9686
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-21 10:10:26 -0700

    code update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,9 +10,13 @@ BaseCaching = __import__("base_caching").BaseCaching
 class BasicCache(BaseCaching):
     def put(self, key, item):
         """
+        put function for basic caching
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        get function for basic caching
+        """
         return self.cache_data.get(key, None)
