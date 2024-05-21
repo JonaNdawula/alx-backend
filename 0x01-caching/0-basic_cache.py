d750cb86759f662c681b38740b559b5ed0e06ffc
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-21 10:05:09 -0700

    Code for 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A class BasicCache inheriting from
+the BaseCaching class and is A
+caching system
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        return self.cache_data.get(key, None)
