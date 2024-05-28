5e9be8ebaf7883dc387134509c19adfa1e7901a4
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-28 04:54:34 -0700

    code for 3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This modue contains a function that parameterizes
+templates
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class config(object):
+    """
+    Configuration class for Flask-Babel
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    This function is used to return the lcal language
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index() -> str:
+    """
+    This function is called when the root route  is accessed
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
